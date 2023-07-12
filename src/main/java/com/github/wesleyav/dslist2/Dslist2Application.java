823db9c0c454bdcbf003cc4bf8dbe5ce04cc6a73
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2023-07-12 14:11:07 -0300

    Adiciona estrutura do projeto

---
+++ src/main/java/com/github/wesleyav/dslist2/Dslist2Application.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.dslist2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dslist2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dslist2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/wesleyav/dslist2/Dslist2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.dslist2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dslist2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
